f2c606e36406ec4ccbba27acee9ec858bba6b0f7
aliozkum <aliozkum@piton.com.tr>
2021-09-10 17:20:47 +0300

    1 sass 7-1 architechture add, 2 highlights component add, 3 slider add

---
+++ js/carousel.js
@@ -0,0 +1,6 @@
+let pagination = document.querySelector('.pagination__item')
+
+pagination.addEventListener('click', (e) => {
+    e.preventDefault()
+    alert(e)
+})
--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 let barBtn = document.querySelector('#barBtn')
-let smallMenu = document.querySelector('.small-menu')
+let smallMenu = document.querySelector('.menu__small')
 
 barBtn.addEventListener('click', () => {
     smallMenu.classList.toggle('show')
